ef0ae32bc9ed31c69e4cf52a34a31135ae806519
Dimitar Todev <35083347+lordviet@users.noreply.github.com>
2023-06-26 14:08:14 +0300

    add multiplication implementation in assembly

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,33 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+    @i // access memory
+    M=1 // assign it value 1
+
+    @R2 // access memory of where product will be stored
+    M=0 // assign it value 0    
+
+(LOOP)
+    @i
+    D=M // D=i
+    
+    @R1 // Whatever is stored in R1
+    D=D-M
+    
+    @END
+    D;JGT // If (i-R1)>0 goto END
+
+    @R0
+    D=M // D=R0
+    
+    @R2 // product
+    M=D+M // R2 = R2 + R0
+
+    @i
+    M=M+1 // i = i + 1
+
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
