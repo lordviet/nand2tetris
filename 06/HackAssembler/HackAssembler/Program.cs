42efbc2e43e916f541a4322c44ccac88c2cf2d3e
Dimitar Todev <35083347+lordviet@users.noreply.github.com>
2023-07-16 15:39:15 +0300

    sample C-instruction handler

--- 06/HackAssembler/HackAssembler/Program.cs
+++ 06/HackAssembler/HackAssembler/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using HackAssembler.Enums;
+using HackAssembler.Implementations;
 
 namespace HackAssembler;
 
@@ -18,6 +20,7 @@ class Program
         string fileContents = File.ReadAllText("/Users/dmtodev/Desktop/Projects/nand2tetris/06/add/Add.asm");
 
         Parser parser = new Parser(fileContents);
+        MnemonicsConverter converter = new MnemonicsConverter();
 
         StringBuilder sb = new StringBuilder();
 
@@ -27,26 +30,59 @@ class Program
 
             if (currentInstructionType == CommandType.A)
             {
-                // TODO extract as method
                 string symbol = parser.Symbol();
 
-                int value = int.Parse(symbol);
+                string aInstructionBits = HandleAInstruction(symbol);
 
-                string binary = Convert.ToString(value, 2);
+                sb.Append(aInstructionBits);
+            }
 
-                int zeroPadCount = 16 - binary.Length;
+            if(currentInstructionType == CommandType.C)
+            {
+                string? destination = parser.Destination();
+                string computation = parser.Computation();
+                string? jump = parser.Jump();
 
-                string padding = new('0', zeroPadCount);
+                string cInstructionBits = HandleCInstruction(converter, destination, computation, jump);
 
-                string converted = $"{padding}{binary}{Environment.NewLine}";
-
-                sb.Append(converted);
+                sb.Append(cInstructionBits);
             }
 
             parser.Advance();
         }
 
         Console.WriteLine(sb.ToString());
+    }
+
+    private static string HandleAInstruction(string symbol)
+    {
+        int value = int.Parse(symbol);
+
+        string binary = Convert.ToString(value, 2);
+
+        int zeroPadCount = 16 - binary.Length;
+
+        string padding = new('0', zeroPadCount);
+
+        string converted = $"{padding}{binary}{Environment.NewLine}";
+
+        return converted;
+    }
+
+    private static string HandleCInstruction(MnemonicsConverter converter, string? destination, string computation, string? jump)
+    {
+        string aBit = "1"; // TODO method to retrieve the a-bit
+
+        string destinationBinary = converter.Destination(destination);
+        string computationBinary = converter.Computation(computation);
+        string jumpBinary = converter.Jump(jump);
+
+        const int onePadCount = 3;
+        string padding = new('1', onePadCount);
+
+        string converted = $"{padding}{aBit}{computationBinary}{destinationBinary}{jumpBinary}{Environment.NewLine}";
+
+        return converted;
     }
 }
 
