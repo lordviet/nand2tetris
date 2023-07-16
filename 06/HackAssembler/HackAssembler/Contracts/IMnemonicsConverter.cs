2fd084961754cf77aa2172d5aae321ef6821287c
Dimitar Todev <35083347+lordviet@users.noreply.github.com>
2023-07-16 03:28:03 +0300

    add mnemonics converter contract

---
+++ 06/HackAssembler/HackAssembler/Contracts/IMnemonicsConverter.cs
@@ -0,0 +1,12 @@
+﻿namespace HackAssembler.Contracts
+{
+    public interface IMnemonicsConverter
+	{
+		string Destination(string mnemonic);
+
+		string Computation(string mnemonic);
+
+		string Jump(string mnemonic);
+	}
+}
+
