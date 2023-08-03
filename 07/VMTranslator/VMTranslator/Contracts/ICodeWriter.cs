f153bcebb8a39744792800f70eadf19344354fe4
Dimitar Todev <35083347+lordviet@users.noreply.github.com>
2023-08-04 00:33:54 +0300

    remove SetFileName from ICodeWriter

--- 07/VMTranslator/VMTranslator/Contracts/ICodeWriter.cs
+++ 07/VMTranslator/VMTranslator/Contracts/ICodeWriter.cs
@@ -4,13 +4,11 @@ namespace VMTranslator.Contracts
 {
     public interface ICodeWriter
 	{
-		void SetFileName(string fileName);
-
 		void WriteCommentedOutInstruction(string instrction);
 
 		void WriteArithmetic(string command);
 
-		void WritePushPop(CommandType command, string segment, int index);
+		void WritePushPop(CommandType commandType, string segment, int index);
 
 		string Close();
 
