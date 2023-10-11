685fbee175089a6b9c51fda298b6018ba57478d5
Dimitar Todev <35083347+lordviet@users.noreply.github.com>
2023-10-12 01:42:23 +0300

    introduce exceptions

---
+++ 10/JackAnalyzer/JackAnalyzer/Exceptions/InvalidTokenException.cs
@@ -0,0 +1,11 @@
+﻿namespace JackAnalyzer.Exceptions
+{
+    public class InvalidTokenException : FormatException
+    {
+        public InvalidTokenException(string token) : base($"The current token - {token} does not match any known format.")
+        {
+        }
+    }
+
+}
+
--- 10/JackAnalyzer/JackAnalyzer/Exceptions/UnexpectedTokenTypeException.cs
+++ 10/JackAnalyzer/JackAnalyzer/Exceptions/UnexpectedTokenTypeException.cs
@@ -1,18 +1,20 @@
-﻿namespace JackAnalyzer.Exceptions
+﻿using JackAnalyzer.Enums;
+
+namespace JackAnalyzer.Exceptions
 {
     public class UnexpectedTokenTypeException : Exception
-	{
+    {
         public UnexpectedTokenTypeException()
         {
         }
 
-        public UnexpectedTokenTypeException(string message)
-        : base(message)
+        public UnexpectedTokenTypeException(TokenType expected, TokenType received)
+        : base($"Expected token of type - {expected}, instead got {received}.")
         {
         }
 
-        public UnexpectedTokenTypeException(string message, Exception inner)
-            : base(message, inner)
+        public UnexpectedTokenTypeException(TokenType expected, TokenType received, Exception inner)
+            : base($"Expected token of type - {expected}, instead got {received}. {inner}")
         {
         }
     }
