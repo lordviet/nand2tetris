d5fcf8df92c627bc1ed0fdac0a6ff6e1724e3c68
Dimitar Todev <35083347+lordviet@users.noreply.github.com>
2023-10-16 01:47:35 +0300

    add unit test setup

---
+++ 10/JackAnalyzer/JackAnalyzer.UnitTests/CompilationEngineTests.cs
@@ -0,0 +1,32 @@
+﻿using JackAnalyzer.Contracts;
+using JackAnalyzer.Implementations;
+
+namespace JackAnalyzer.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestWhile()
+    {
+        string whileExpression = """
+        while (i < length) {
+            let sum = sum + a[i];
+            let i = i + 1;
+        }   
+        """;
+
+        IJackTokenizer tokenizer = new JackTokenizer(whileExpression);
+        ICompilationEngine engine = new CompilationEngine(tokenizer);
+
+        engine.CompileWhile();
+
+        string compiled = engine.Close();
+
+        Assert.NotNull(compiled);
+    }
+}
