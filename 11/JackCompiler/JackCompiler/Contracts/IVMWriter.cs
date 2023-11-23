f341871ce2289989f7f8e8e0413e3280bc3126b7
Dimitar Todev <35083347+lordviet@users.noreply.github.com>
2023-11-23 02:20:43 +0200

    add VM Writer contract

---
+++ 11/JackCompiler/JackCompiler/Contracts/IVMWriter.cs
@@ -0,0 +1,26 @@
+﻿using JackCompiler.Enums;
+
+namespace JackCompiler.Contracts
+{
+    public interface IVMWriter
+    {
+        public void WritePush(Segment segment, int index);
+
+        public void WritePop(Segment segment, int index);
+
+        public void WriteArithmetic(Command command, int index);
+
+        public void WriteLabel(string label);
+
+        public void WriteGoto(string label);
+
+        public void WriteIf(string label);
+
+        public void WriteCall(string name, int nArgs);
+
+        public void WriteFunction(string name, int nLocals);
+
+        public void WriteReturn();
+    }
+}
+
